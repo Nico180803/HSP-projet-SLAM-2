9ed89803505c096215ddb1db1e93294c8db33535
Théo Enfedaque <theo.enf@hoziodev.fr>
2025-09-30 11:56:24 +0200

    feat : ajout page support avec formulaire + envoie de mail surement fonctionnel quand l'addresse mail sera fonctionnel

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,8 +5,14 @@ namespace App\Controller;
 
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -19,4 +25,29 @@ class HomeController extends AbstractController
         return $this->render('home/index.html.twig');
     }
 
+    #[Route('/support', name: 'app_support')]
+    public function support(Request $request, MailerInterface $mailer): Response
+    {
+        $form = $this->createFormBuilder()
+            ->add('email', EmailType::class)
+            ->add('sujet', TextType::class)
+            ->add('demande', TextareaType::class)
+            ->add('submit', SubmitType::class)
+            ->getForm();
+        $form->handleRequest($request);
+
+        //dd($form->getData());
+
+        /*if ($form->isSubmitted() && $form->isValid()) {
+            $email = (new Email())
+                ->from('support.hsp@hoziodev.fr')
+                ->to($form->getData()['email'])
+                ->subject($form->getData()['sujet'])
+                ->text($form->getData()['demande']);
+            $mailer->send($email);
+        }*/
+        return $this->render('home/support.html.twig', [
+            'demande' => $form
+        ]);
+    }
 }
